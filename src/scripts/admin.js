50005a1c049f430492d34969aa23634b34506b18
Isac-d <isac.danielsson@yh.nackademin.se>
2025-03-27 14:02:00 +0100

    popup for product with placeholder text

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -53,3 +53,15 @@ window.onclick = function(event) {
         closePopup();
     }
 };
+
+document.addEventListener('click', (event) => {
+    // If the target is a button, do nothing (prevent event propagation)
+    if (event.target.tagName.toLowerCase() === 'button') {
+        return;
+    }
+    // Check if the click is on a .product (or its descendants)
+    const product = event.target.closest('.product');
+    if (product) {
+        openPopup("mainOverlay", ".popup-content.single-product");
+    }
+});
